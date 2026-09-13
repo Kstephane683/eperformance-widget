473e7cd5195f9489dc33f76bf791e306532faeb6
K. Stéphane <stephane@eperformance.pro>
2026-09-13 08:13:09 +0200

    Updates

--- eperformance-sdk.js
+++ eperformance-sdk.js
@@ -50,9 +50,12 @@ var ePerformanceSDK=(function(e){Object.defineProperty(e,Symbol.toStringTag,{val
   #${a} {
     right: 0 !important;
     left: 0 !important;
+    top: 0 !important;
     bottom: 0 !important;
     width: 100% !important;
-    height: 100dvh !important;
+    /* 100% (viewport exact) plutôt que 100dvh: dvh inclut les barres
+       rétractables du navigateur → débordement haut (header coupé) */
+    height: 100% !important;
     max-width: 100%;
     border-radius: 0;
   }
